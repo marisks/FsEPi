62d21c63f1f97c797b05c63a7f6e176e4b0f8878
Māris Krivtežs <marisks@gmail.com>
2014-07-30 23:21:56 +0300

    Start page

---
+++ FsEPi/Models.fs
@@ -0,0 +1,8 @@
+﻿namespace FsEPi.Models
+
+open EPiServer.Core
+open EPiServer.DataAnnotations
+
+[<ContentType(DisplayName = "Start page", GUID = "2048D6E0-8D23-455B-A687-D6321F852514", Order = 10, AvailableInEditMode = true)>]
+type StartPage() =
+    inherit PageData()
