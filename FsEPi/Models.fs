75e4d7c824dbda779dc8dff72de3e03622d1c7f1
Māris Krivtežs <marisks@gmail.com>
2014-07-30 23:44:05 +0300

    Added heading property to start page

--- FsEPi/Models.fs
+++ FsEPi/Models.fs
@@ -1,8 +1,13 @@
 ﻿namespace FsEPi.Models
 
+open System.ComponentModel.DataAnnotations
 open EPiServer.Core
 open EPiServer.DataAnnotations
 
 [<ContentType(DisplayName = "Start page", GUID = "2048D6E0-8D23-455B-A687-D6321F852514", Order = 10, AvailableInEditMode = true)>]
 type StartPage() =
     inherit PageData()
+
+    abstract Heading : string with get, set
+    [<Display(Name = "Some heading")>] // TODO: place attributes on implementation
+    default val Heading = "" with get, set
