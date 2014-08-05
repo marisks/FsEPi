d560b8b5155fbf29afe9891ec19eb4e4b0e5f696
Māris Krivtežs <marisks@gmail.com>
2014-08-05 22:36:41 +0300

    Added basic controller and views

---
+++ FsEPi/Controllers.fs
@@ -0,0 +1,12 @@
+﻿namespace FsEPi.Controllers
+
+open System.Diagnostics
+open System.Web.Mvc
+open EPiServer.Web.Mvc
+open FsEPi.Models
+
+type StartPageController() =
+    inherit PageController<StartPage>()
+
+    member x.Index(currentPage : StartPage) =
+        x.View(currentPage)
