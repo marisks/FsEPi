51881b5071a880e1bb5ab972a2b3388501240847
Māris Krivtežs <marisks@gmail.com>
2014-08-15 20:02:28 +0300

    Testing out different strategies for getting page type values

--- FsEPi/Controllers.fs
+++ FsEPi/Controllers.fs
@@ -1,12 +1,19 @@
 ﻿namespace FsEPi.Controllers
 
-open System.Diagnostics
-open System.Web.Mvc
+open EPiServer
+open EPiServer.ServiceLocation
 open EPiServer.Web.Mvc
 open FsEPi.Models
 
 type StartPageController() =
     inherit PageController<StartPage>()
 
-    member x.Index(currentPage : StartPage) =
-        x.View(currentPage)
+    member x.Index(currentPage:StartPage) =
+        let contentRepo = ServiceLocator.Current.GetInstance<IContentRepository>()
+        // Trying out to get property values by calling repo, but still same result
+        // page.Property (property on ContentData base class) contains values, but StartPage properties are not set
+        let page = contentRepo.Get<StartPage>(currentPage.ContentLink)
+        x.View(page)
+
+    member x.GetCurrentPage() =
+        base.PageContext.Page :?> StartPage
--- FsEPi/Models.fs
+++ FsEPi/Models.fs
@@ -4,10 +4,18 @@ open System.ComponentModel.DataAnnotations
 open EPiServer.Core
 open EPiServer.DataAnnotations
 
-[<ContentType(DisplayName = "Start page", GUID = "2048D6E0-8D23-455B-A687-D6321F852514", Order = 10, AvailableInEditMode = true)>]
+[<ContentType(DisplayName = "Start page", GUID = "2048D6E0-8D23-455B-A687-D6321F852514")>]
 type StartPage() =
     inherit PageData()
 
     abstract Heading : string with get, set
     [<Display(Name = "Some heading")>] // placing attributes on implementation
     default val Heading = "" with get, set
+
+    abstract IAmNew : string with get, set
+    [<Display(Name = "I am new")>] // placing attributes on implementation
+    default val IAmNew = "" with get, set
+    
+    abstract IAmNewBool : bool with get, set
+    [<Display(Name = "I am new bool")>] // placing attributes on implementation
+    default val IAmNewBool = false with get, set
